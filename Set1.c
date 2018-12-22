2ec68b1d04d1cc7418e2eacd0151e2dcc145e0c5
Saranyaeee21128 <46068687+Saranyaeee21128@users.noreply.github.com>
2018-12-22 18:32:29 +0530

    Set1.c

---
+++ Set1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.>
+Void main()
+{ 
+ Int n;
+ Clrscr();
+ Printf("enter the num:");
+ Scanf("%d",&n);
+ If(n>0)
+ {
+ Printf("positive num");
+ }
+ Else
+ {
+ Printf("negative num");
+ }
+Getch()
+}
